378a6c3786d13987f1f527bbcdea179ef69c0ee7
kikk01 <hugobar@live.fr>
2021-02-20 10:13:40 +0100

    ch2 test with bdd

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\DataFixtures\UserFixtures;
+use App\Repository\UserRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserRepositoryTest extends KernelTestCase
+{
+
+    use FixturesTrait;
+
+    public function testCount() {
+        $users = $this->loadFixtureFiles([
+            __DIR__ . '/UserRepositoryTestFixtures.yaml'
+        ]);
+
+        $users = self::$container->get(UserRepository::class)->count([]);
+
+        $this->assertEquals(10, $users);        
+
+    }
+}
